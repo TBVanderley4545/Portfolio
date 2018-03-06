5a7fc740367ab521c376b52b2809e2252c12d263
TBVanderley4545 <TBVanderley4545@users.noreply.github.com>
2018-03-05 23:04:45 -0500

    Change how the theme is pulled in.

--- wp-content/themes/twentyseventeen-ty/functions.php
+++ wp-content/themes/twentyseventeen-ty/functions.php
@@ -584,3 +584,8 @@ require get_parent_theme_file_path( '/inc/customizer.php' );
  * SVG icons functions and filters.
  */
 require get_parent_theme_file_path( '/inc/icon-functions.php' );
+
+add_action( 'wp_enqueue_scripts', 'enqueue_parent_styles' );
+function enqueue_parent_styles() {
+    wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' );
+}
