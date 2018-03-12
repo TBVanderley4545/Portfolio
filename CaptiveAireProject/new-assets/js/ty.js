49a015a522e03e5a627209421cd3151f07980662
TBVanderley4545 <TBVanderley4545@users.noreply.github.com>
2018-03-11 20:42:22 -0400

    Remove the class on mouse out.

--- CaptiveAireProject/new-assets/js/ty.js
+++ CaptiveAireProject/new-assets/js/ty.js
@@ -1,26 +1,23 @@
 $(document).ready(function () {
 
   let thermometerIcon = document.getElementById("thermometer-icon");
+  let thermometerFill = document.getElementsByClassName("thermometer-fill")[0];
 
-  thermometerIcon.addEventListener("mouseover", function() {
-    let thermometerFill = document.getElementsByClassName("thermometer-fill");
-
-    thermometerFill[0].classList.add("thermometer-fill-active");
-  });
 
   // This code is being used to target the thermometer icon animation.
-  $('#thermometer-icon').hover(
-    function () {
-      if (!$('.thermometer-fill').hasClass('thermometer-fill-active')) {
-        $('.thermometer-fill').addClass('thermometer-fill-active')
-      }
-    },
-    function () {
-      if ($('.thermometer-fill').hasClass('thermometer-fill-active')) {
-        $('.thermometer-fill').removeClass('thermometer-fill-active');
-      }
+  thermometerIcon.addEventListener("mouseover", function () {
+
+    if (thermometerFill.classList.contains("thermometer-fill-active")) {
+      thermometerFill.classList.add("thermometer-fill-active");
     }
-  );
+  });
+
+  thermometerIcon.addEventListener("mouseout", function () {
+    if (!thermometerFill.classList.contains("thermometer-fill-active")) {
+      thermometerFill.classList.remove("thermometer-fill-active");
+    }
+  });
+
 
   /********************
   HELPER FUNCTIONS
