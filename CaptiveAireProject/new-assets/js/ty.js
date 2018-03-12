fff3631b63f10e9f539a48409ced3159d163572b
TBVanderley4545 <TBVanderley4545@users.noreply.github.com>
2018-03-11 21:14:43 -0400

    We're going to stick to Plain Javascript.

--- CaptiveAireProject/new-assets/js/ty.js
+++ CaptiveAireProject/new-assets/js/ty.js
@@ -1,35 +1,21 @@
 $(document).ready(function () {
 
-  // let thermometerIcon = document.getElementById("thermometer-icon-container");
-  // let thermometerFill = document.getElementsByClassName("thermometer-fill")[0];
+  let thermometerIcon = document.getElementById("thermometer-icon-container");
+  let thermometerFill = document.getElementsByClassName("thermometer-fill")[0];
 
 
-  // // This code is being used to target the thermometer icon animation.
-  // thermometerIcon.addEventListener("mouseover", function () {
+  // This code is being used to target the thermometer icon animation.
+  thermometerIcon.addEventListener("mouseover", function () {
 
-  //   if (!thermometerFill.classList.contains("thermometer-fill-active")) {
-  //     thermometerFill.classList.add("thermometer-fill-active");
-  //   }
-  // });
-
-  // thermometerIcon.addEventListener("mouseout", function () {
-  //   if (thermometerFill.classList.contains("thermometer-fill-active")) {
-  //     thermometerFill.classList.remove("thermometer-fill-active");
-  //   }
-  // });
-
-  $("#thermometer-icon-container").hover(function() {
-    if(!$(".thermometer-fill").hasClass("thermometer-fill-active")) {
-      $(".thermometer-fill").addClass("thermometer-fill-active");
+    if (!thermometerFill.classList.contains("thermometer-fill-active")) {
+      thermometerFill.classList.add("thermometer-fill-active");
     }
-  }, function() {
-    if($(".thermometer-fill").hasClass("thermometer-fill-active")) {
-      $(".thermometer-fill").removeClass("thermometer-fill-active");
-    }  });
+  });
 
-
-  /********************
-  HELPER FUNCTIONS
-  ********************/
+  thermometerIcon.addEventListener("mouseout", function () {
+    if (thermometerFill.classList.contains("thermometer-fill-active")) {
+      thermometerFill.classList.remove("thermometer-fill-active");
+    }
+  });
 
 });
